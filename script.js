0135d3a778306487583c86feeb96bd710bdd71d6
Shaurya kumar <shauryakumar682@gmail.com>
2025-07-24 16:44:09 +0530

    Certificate section

---
+++ script.js
@@ -0,0 +1,9 @@
+function showSkills() {
+        document.getElementById("skills-section").style.display = "flex";
+        document.getElementById("certificate-section").style.display = "none";
+    }
+
+    function showCertificates() {
+        document.getElementById("skills-section").style.display = "none";
+        document.getElementById("certificate-section").style.display = "flex";
+    }
